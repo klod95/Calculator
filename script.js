84c845d90e64cbd056b60fb427c3287f53455b5b
klod95 <vagabondergr@gmail.com>
2023-12-22 16:06:46 +0100

    first commit. added some simple css,html

---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = 0;
+oper = chosenOper;
+let num2 = 0
+const add = document.getElementsByClassName("add");
+const subtract = document.getElementsByClassName("subtract");
+const multiply = document.getElementsByClassName("multiply");
+const divide = document.getElementsByClassName("divide");
+const screen = document.getElementsByClassName("screen");
+
+function operator(num1,oper,num2){
+
+}
